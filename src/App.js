68eb84281e11a3a6b37281af0730aba1a287460c
elin-oh <oh_ji_0@kakao.com>
2020-11-11 16:39:45 +0900

    [client] project init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import { List, Shorten, Home, Login,User } from './pages';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Route exact path="/" component={} />
+        <Switch>
+          <Route path="/list" component={List} />
+        </Switch> */}
+      </div>
+    );
+  }
+}
+
+export default App;
