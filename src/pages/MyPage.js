bcb93ae037e0759c116b71b8b9232ae2e8f72452
ggomajae <ggomajae@gmail.com>
2020-11-12 20:20:11 +0900

    [Client] add Header component

---
+++ src/pages/MyPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from 'components/Header';
+
+class Mypage extends Component {
+  render() {
+    return (
+      <div className="App-wrap">
+        <div className="mb-view">
+          <Header />
+        </div>
+      </div >
+    );
+  }
+}
+export default Mypage;
