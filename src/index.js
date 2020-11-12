c49a507f24f4f147af61ddcdf081f8d34cca87f8
coldplay126 <68833110+coldplay126@users.noreply.github.com>
2020-11-12 11:43:57 +0900

    [client] completed redux setting

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import reducer from './reducers';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
+
+const composeEnhancers = composeWithDevTools({
+  // options like actionSanitizer, stateSanitizer
+});
+
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(),
+));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
