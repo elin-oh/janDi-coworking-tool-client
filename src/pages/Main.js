88bce698671d2de461e2ee10c61f597747a76349
ggomajae <ggomajae@gmail.com>
2020-11-13 14:21:18 +0900

    [client] fix App-content css

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,20 @@
-import React from 'react';
-
-export default function Main() {
-  return (
-    <div className="App-wrap jandiBg">
-      <div className="mb-view">
+import React, { Component } from 'react';
+import Header from 'components/Header';
+import Button from 'components/Button';
+class Main extends Component {
+  render() {
+    return (
+      <div className="App-wrap">
+        <div className="mb-view">
+          <Header />
+          <div className="App-contents">
+            <h2>프로젝트 리스트</h2>
+            <Button>프로젝트 생성하기</Button>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Main;
