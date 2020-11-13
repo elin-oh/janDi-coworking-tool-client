cf58a32f9b1c026893e6ef2a79687f5743768519
ggomajae <ggomajae@gmail.com>
2020-11-13 11:48:37 +0900

    [client] component Popup add

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Popup.scss';
+
+class Popup extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    // props에 open설정시 팝업 오픈
+    if (this.props.open) {
+      return (
+        // Props에 설정 필수값: min-height, height
+        <div className="PopupWrapper">
+          {/* 팝업 컴포넌트 사용시 props에 onClosePopup 메소드를 연결해주세요 */}
+          <div className="btnClosePopup" onClick={this.props.onClosePopup}>
+            <img src="/img/btn_close_popup.png" alt="팝업닫기" />
+          </div>
+          {this.props.children}
+        </div>
+      );
+    } else {
+      return null;
+    }
+
+  }
+}
+
+
+
+export default Popup;
