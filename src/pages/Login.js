70ab4f4f2b817ace184c2e845309a4a0abb8ec1d
coldplay126 <68833110+coldplay126@users.noreply.github.com>
2020-11-15 15:47:02 +0900

    [client]keep setting login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import styles from 'styles/login.css';
 import { Link } from 'react-router-dom';
+import Button from 'components/Button'
+//import Fblogin from 'components/Fblogin' 페이스북 로그인
 
 const cx = classNames.bind(styles);
 
@@ -17,20 +19,26 @@ class Login extends Component {
             errorMessage: "",
             test: ""
         };
+
+
     }
 
 
     handleInputValue = (key) => (e) => {
         this.setState({ [key]: e.target.value });
+
     };
 
-    handleLogin() {
-        const { userId, password } = this.state
+    handleLogin = () => {
+        const { userId, password } = this.state;
+        // const { handleResponseSuccess } = this.props;
 
         if (!userId || !password) {
-            this.setState({ errorMessage: "이메일과 비밀번호를 입력하세요" })
+            console.log("====================", this.state)
+            this.setState({ errorMessage: "아이디 또는 비밀번호를 입력하세요." })
         } else {
-            return true;
+            console.log("====================", this.state)
+            this.props.history.push("/Mypage")
         }
     };
 
@@ -39,28 +47,34 @@ class Login extends Component {
         return (
             <div className="App-wrap">
                 <div className="mb-view">
-                    <img src="/img/jandi_logo.png" alt="jandi_logo" className="logo" />
+                    <img className="loginLogo" src="/img/jandi_logo.png" alt="jandi_logo" />
                     <div className="App-contents">
                         <h2>잔디에 오신걸 환영합니다</h2>
                         <ul className={cx('login-userInfo')}>
-                            <li>
-                                <div className="inputWrap">
-                                    <input type='text' placeholder="아이디를 입력하세요" onChange={this.handleInputValue("userId")}></input>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="inputWrap">
-                                    <input type="password" placeholder="비밀번호를 입력하세요" />
-                                </div>
-                            </li>
-                            <button className='btn btn-login' type='submit' onClick={this.handleLogin}>
-                                로그인
-                            </button>
-                            {<div className="alert-box">{this.state.errorMessage}</div>}
-                            <div className="inputWrap">
-                                <Link to="/signUp">계정이 없으신가요?</Link>
+                            <form onSubmit={(e) => e.preventDefault()}>
+                                <li>
+                                    <div className="inputWrap">
+                                        <input type='text' placeholder="아이디를 입력하세요" onChange={this.handleInputValue("userId")}></input>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="inputWrap">
+                                        <input type="password" onChange={this.handleInputValue("password")} placeholder="비밀번호를 입력하세요" />
+                                    </div>
+                                </li>
+                            </form>
+                            <div onClick={this.handleLogin.bind(this)}>
+                                {<div style={{ color: "red" }} >{this.state.errorMessage}</div>}
+                                <Button bgColor='#b8B8B8' color="white" mdSize="full" >로그인</Button>
                             </div>
                         </ul>
+                        <div className="goToSignUp">
+                            {/*페이스북 로그인 
+                            <p>또는</p>
+                            <Fblogin />
+                            */}
+                            <Link to="/signUp">계정이 없으신가요?</Link>
+                        </div>
                     </div>
                 </div>
             </div >
